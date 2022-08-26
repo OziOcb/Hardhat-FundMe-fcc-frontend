e70103dd50e5c94efeb3b933bfab47e8402b7ac8
OziOcb <pozyzniewski@yahoo.com>
2022-08-26 15:34:13 +0100

    Before - Sending a Transaction From a Website - 13:07:57

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+// Importuje ethers w ten sposob tylko kiedy uzywam czystego JS
+// We frameworkach instaluje ethers z npm i importuje normalnie
+// jak kazda inna paczke
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+// prettier-ignore
+console.log("-\n--\n ethers \n >", ethers, "\n--\n-") // REMOVE_ME: remove when done!
+
 // Sprawdzam czy wtyczka Metamask jest zainstalowana w przegladarce
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     try {
       // Wysylam request o polaczenie z dostepnymi kontami w Metamasku
-      await window.ethereum.request({ method: "eth_requestAccounts" });
+      await window.ethereum.request({ method: "eth_requestAccounts" })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    document.getElementById("connectButton").innerHTML = "Connected!";
+    connectButton.innerHTML = "Connected!"
   } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please Install Metamask!";
+    connectButton.innerHTML = "Please Install Metamask!"
+  }
+}
+
+async function fund(ethAmount) {
+  if (typeof window.ethereum !== "undefined") {
+    // Rzeczy ktorych potrzebuje
+    // provider / czyli placzenie z blockchainem
+    // signer / wallet / czyli cos za czego zaplace za gaz
+    // contract / z tego bede potrzebowal ABI i Address
   }
 }
